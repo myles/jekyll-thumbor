4fc96318b3ae96820a0a2eb2d8aea1967f77dabb
Myles Braithwaite <me@mylesbraithwaite.com>
2017-06-22 11:31:16 -0400

    :green_heart: Issue with how the version number was handled.

--- lib/jekyll/thumbor_tag.rb
+++ lib/jekyll/thumbor_tag.rb
@@ -2,33 +2,33 @@ require "liquid"
 require "ruby-thumbor"
 
 module Jekyll
-  class ThumborTag < Liquid::Tag
+  module ThumborTag
+    class ThumborTag < Liquid::Tag
 
-    VERSION = "0.3.0"
+      def initialize(tag_name, text, tokens)
+        super
 
-    def initialize(tag_name, text, tokens)
-      super
+        tokens = text.split /\,\s/
+        @url = tokens[0]
+        @parameters = {}
 
-      tokens = text.split /\,\s/
-      @url = tokens[0]
-      @parameters = {}
-
-      tokens[1..-1].each do |arg|
-        k, v = arg.split /:/
-        v ||= "1"
-        @parameters[k.strip] = v.strip
+        tokens[1..-1].each do |arg|
+          k, v = arg.split /:/
+          v ||= "1"
+          @parameters[k.strip] = v.strip
+        end
       end
-    end
 
-    def render(context)
-      @config = context.registers[:site].config['thumbor']
+      def render(context)
+        @config = context.registers[:site].config['thumbor']
 
-      image = Thumbor::Cascade.new(@config['key'], @url)
-      image_url = image.width(@parameters['width']).height(@parameters['height']).generate
+        image = Thumbor::Cascade.new(@config['key'], @url)
+        image_url = image.width(@parameters['width']).height(@parameters['height']).generate
 
-      "#{@config['url']}#{image_url}"
+        "#{@config['url']}#{image_url}"
+      end
     end
   end
 end
 
-Liquid::Template.register_tag('thumbor', Jekyll::ThumborTag)
+Liquid::Template.register_tag('thumbor', Jekyll::ThumborTag::ThumborTag)
---
+++ lib/jekyll/thumbor_tag/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module ThumborTag
+    VERSION = "0.3.0"
+  end
+end
